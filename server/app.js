35d738129dc36d5d7267c00d57c1c2ee06476cf2
Oganesyan Artur <arturris@mail.ru>
2018-10-23 21:53:21 -0500

    Mongoose models for mLab

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
 
 const schema = require('./schema/schema');
+const MLAB_CRED = require('./credentials');
+const {login, password} = MLAB_CRED;
 
 const app = express();
+
+mongoose.connect(`mongodb://${login}:${password}@ds153699.mlab.com:53699/react-planner`,  {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log('Connected to DB');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema, // === schema: schema 
