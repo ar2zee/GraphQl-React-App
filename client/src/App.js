143126ac9dc114f7c186be8d314eb72eb867008d
Oganesyan Artur <arturris@mail.ru>
2018-11-28 20:13:02 -0600

    Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import AppoloClient from 'apollo-boost';
+import {AppoloProvider} from 'react-apollo';
 
 import BookList from './components/BookList';
+
+const client = AppoloClient({
+  uri: "http://localhost:4000/graphql"
+})
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Test h1</h1>
-        <BookList />
-      </div>
+      <AppoloProvider client={client}>
+        <div id="main">
+          <h1>Test h1</h1>
+          <BookList />
+        </div>
+      </AppoloProvider>
     );
   }
 }
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const schema = require('./schema/schema');
@@ -8,6 +9,8 @@ const MLAB_CRED = require('./credentials');
 const {login, password} = MLAB_CRED;
 
 const app = express();
+
+xapp.use(cors());
 
 mongoose.connect(`mongodb://${login}:${password}@ds153699.mlab.com:53699/react-planner`,  {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
